3d786b082a122105d08e0382edff9c5884c38a2a
daniscodes <dan97@bell.net>
2022-08-04 01:21:58 -0400

    Cleaned up

--- timer1.js
+++ timer1.js
@@ -1,15 +1,13 @@
 const timer = function (times) {
-
-  if (!times.length) return;
- 
-    for (let time of times){
-      const num = Number(time)
-      setTimeout(() => {
-        console.log("BEEP at " + num + " seconds")
-        process.stdout.write('\x07');
-      }, num * 1000)
-    }
+  for (let time of times) {
+    const num = Number(time)
+    if (time < 0) continue;
+    if (isNaN(time)) continue;
+    setTimeout(() => {
+      console.log("BEEP at " + num + " seconds")
+      process.stdout.write('\x07');
+    }, num * 1000)
   }
- 
-  const times = process.argv.slice(2)
-  timer(times)
+}
+const times = process.argv.slice(2)
+timer(times)
